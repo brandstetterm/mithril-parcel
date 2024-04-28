1038e36c1bc30c7d96fa7c2369ae9034f237f980
Manuel Brandstetter <accounts@manuelbrandstetter.dev>
2024-04-28 21:22:08 +0200

    Add classes to app.ts components as test

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import m from 'mithril';
 var RootComponent = {
   view: function() {
     return m("main", [
-      m("h1", "Hello World"),
+      m("h1", {class: "text-3xl blue"}, "Hello World"),
       m("p", "Lorem Ipsum")
     ])
   }
